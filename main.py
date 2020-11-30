58024da1a6b5477ab362c9f6339c70f54fffc2da
Igoptt <rodrigo.severim.10@gmail.com>
2020-11-30 19:52:22 +0000

    pseudo codigo para começar o robo

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ ev3.speaker.beep()
 roboot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 #verificar as medidas das rodas 
 
+#inicializa o sensor de cores (verificar o port no robo)
+color_sensor = ColorSensor(Port.D)
 
+#posiçao inicial do robo
+x = 0
+y = 0
+posicao_robo = [x,y] #falta denotar os limites do tabuleiro ou seja o X onde sai e o Y onde sai e tals
+coordenadas_paredes = []
 while True:
     int ovelhas 
     #anda a 200milimetros por segundo
@@ -41,4 +48,31 @@ while True:
     robot.straight(-200)
     #vira 90 graus para verificar a sua direita
     robot.turn(90)
-     #blabla
+
+    #pseudo codigo
+    while True:
+        robo.straight(200) #andou 200 no sentido positivo do eixo do Y
+        posicao_robo[1] = posicao_robo[1] + 200
+        #encontra cor preta ou parede e regista
+        robo.straight(-200)
+        posicao_robo[1] = posicao_robo[1] - 200
+        #vira para a direita e anda no sentido positivo do eixo do X
+        robo.turn(90)
+        robo.straight(200)
+        posicao_robo[0] = posicao_robo[0] + 200
+        #encontra cor preta ou parede e regista
+        posicao_robo[0] = posicao_robo[0] - 200
+        #repete o acima no sentido negativo de X e Y (ou seja vira mais 90 graus anda 200 e a posicao no eixo do Y passa a ser o q era -200 blah blah)
+        #como registar as paredes para o robo se lembrar? podemos guardar a posiçao X e Y da parede mas vamos guardar essas posiçoes onde?
+        #podemos ter um array de arrays que guarda as paredes maybe
+        #integrar a verificaçao das cores, o python tem mains? dava jeito ser outra funçao separada
+
+    #verifica a cor
+    if(color_sensor.color() == Color.BLACK)
+        #regista que aquela borda nao é uma parede e volta para o centro do quadrado
+
+    else if (color_sensor.color() == Color.YELLOW) #(dps mudar para a cor q vamos usar tho nao tem mts q podemos usar)
+        #regista a parede ou seja a sua posiçao atual de X Y passa a ser uma parede
+        #como guardar as posiçoes atuais? possivelmente num array not sure
+        #volta para o centro do quadrado
+        robot.straight(-200) #a distancia do meio do quadrado ate a parte preta no lugar do 200
