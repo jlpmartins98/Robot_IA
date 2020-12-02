81054f389a370f9a9526fe57d444a68e6cd65b68
jlpmartins98 <50104175+jlpmartins98@users.noreply.github.com>
2020-12-02 15:38:45 +0000

    ideias

--- main.py
+++ main.py
@@ -19,26 +19,52 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
+#inicializacao dos sensores 
+touch_sensor = TouchSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
+
 # Write your program here.
-ev3.speaker.beep()
+#ev3.speaker.beep()
 
 #drive base 
 roboot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 #verificar as medidas das rodas 
 
 
-while True:
-    int ovelhas 
-    #anda a 200milimetros por segundo
-    robot.drive(200, 0)
+def ovelhas():
+    int ovelhas
+    while obstacle_sensor.distance() < 300:
+        wait(10)
+        ev3.speaker.beep()
+        ovelhas += 1
 
-        while obstacle_sensor.distance() > 300:
-            wait(10)
-            ev3.speaker.beep()
-            ovelhas++;
-    
-    #robot anda para tras 200mm
-    robot.straight(-200)
-    #vira 90 graus para verificar a sua direita
-    robot.turn(90)
-     #blabla
+def run():
+    while True:
+        cor = color_sensor.color()
+        int limites_cacifo
+        int parede
+
+        #anda a 200milimetros por segundo
+        robot.drive(200, 0)
+        #enquanto nao verificou os 4 limites de um cacifo
+        while (limites_cacifo != 4):
+            #verifica a cor do limite, se for preto, nao tem parede
+            if (cor = Color.BLACK):
+                ovelhas()
+                #robot anda para tras 200mm
+                robot.straight(-200)
+                limites_cacifo += 1
+                #vira 90 graus para verificar a sua direita
+                robot.turn(90)
+        
+            elif (cor = Color.BLUE): #ESCOLHER CORES PRIMARIAS PARA SER MAIS FACIL O ROBO IDENTIFICAR
+                ovelhas()
+                robot.straight(-200)
+                parede += 1
+                robot.turn(90)
+            
+            else:
+
+
+
+            
